0035d10a2c618ff5cbb30ae2ec682e86c0be8a33
eduardoGChavez <druidasco@gmail.com>
2022-08-13 00:52:36 -0500

    [ADD] Parámetros de conexión remota MySQL

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('app', 'root', 'EduardoGarciaChavez', {
-    host: 'localhost',
+const db = new Sequelize('calendar', 'admin', 'asddsa.123321', {
+    host: 'database-calendar.cro8sjffnmiv.us-west-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
